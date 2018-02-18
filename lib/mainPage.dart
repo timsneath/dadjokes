b2891aed48da8a48324ddf8b25853959977022db
Tim Sneath <timsneath@google.com>
2018-02-18 12:54:37 -0800

    add toolbar with about and share options

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -33,48 +33,68 @@ class MainPageState extends State<MainPage> {
     });
   }
 
+  _about() {
+    final aboutDialog = new AlertDialog(
+      title: new Text('About Dad Jokes'),
+      content: new Text('This app is brought to you by Tim Sneath (@timsneath), proud parent of Naomi, Esther, and Silas. May your children groan like mine will.'),
+    );
+    showDialog(context: context, child: aboutDialog);
+    }
+
+  void _share() {
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.info),
+            tooltip: 'About Dad Jokes',
+            onPressed: _about,
+          ),
+          new IconButton(
+            icon: new Icon(Icons.share),
+            tooltip: 'Share joke',
+            onPressed: _share,
+          )
+        ],
       ),
       body: new Center(
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new FutureBuilder<String>(
-                future: _response,
-                builder:
-                    (BuildContext context, AsyncSnapshot<String> snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.none:
-                      return new Icon(Icons.sync_problem);
-                    case ConnectionState.waiting:
-                      return new Center(child: new CircularProgressIndicator());
-                    default:
-                      final decoded = JSON.decode(snapshot.data);
+              future: _response,
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                    return new Icon(Icons.sync_problem);
+                  case ConnectionState.waiting:
+                    return new Center(child: new CircularProgressIndicator());
+                  default:
+                    final decoded = JSON.decode(snapshot.data);
 
-                      if (decoded['status'] == 200) {
-                        return new Text(decoded['joke'],
-                          style: Theme.of(context).textTheme.display1);
-                      } else {
-                        return new Icon(Icons.error);
-
-                      }
-                  }
-                }),
-            new RaisedButton(
-              onPressed: _refresh,
-              child: new Text('New Joke'),
+                    if (decoded['status'] == 200) {
+                      return new Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: new Text(decoded['joke'],
+                              style: Theme.of(context).textTheme.display1));
+                    } else {
+                      return new Icon(Icons.error);
+                    }
+                }
+              },
             ),
           ],
         ),
       ),
       floatingActionButton: new FloatingActionButton(
         onPressed: _refresh,
-        tooltip: 'Share',
-        child: new Icon(Icons.share),
+        tooltip: 'New joke',
+        child: new Icon(Icons.refresh),
       ),
     );
   }
