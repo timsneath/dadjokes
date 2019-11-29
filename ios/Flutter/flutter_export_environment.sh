70f636eda45255c2629870e1dd3c1d3674d465fe
Tim Sneath <timsneath@google.com>
2019-11-29 15:58:17 -0800

    Updated iOS v2.2 to store

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/timsneath/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/timsneath/git/me/dadjokes"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/timsneath/git/me/dadjokes/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/timsneath/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_FRAMEWORK_DIR=/Users/timsneath/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.3.0"
 export "FLUTTER_BUILD_NUMBER=1.3.0"
+export "TRACK_WIDGET_CREATION=true"
