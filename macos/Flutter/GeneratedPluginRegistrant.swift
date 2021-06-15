6dd2623ab4bdbc285d13cefaa743ed3b0e119862
Tim Sneath <timsneath@google.com>
2021-06-15 13:57:42 -0700

    Update dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import window_size
 import path_provider_macos
+import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
